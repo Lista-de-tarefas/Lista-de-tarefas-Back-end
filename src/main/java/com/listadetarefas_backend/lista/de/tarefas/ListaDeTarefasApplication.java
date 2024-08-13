7f12fde8842ba6db8c43075f4747b8d815c72d90
LucasCordeiro-pixel <lucascordeirov2021@gmail.com>
2024-08-13 19:32:13 -0300

    Initial commit

---
+++ src/main/java/com/listadetarefas_backend/lista/de/tarefas/ListaDeTarefasApplication.java
@@ -0,0 +1,13 @@
+package com.listadetarefas_backend.lista.de.tarefas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListaDeTarefasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListaDeTarefasApplication.class, args);
+	}
+
+}
